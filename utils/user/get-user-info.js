17428e5acfbee56cdb33c5753567cf83ca6b6406
Jonathan Chin <jonathankchin@gmail.com>
2020-04-12 00:37:45 -0400

    feat: init commit of module

---
+++ utils/user/get-user-info.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const constants = require('./../../data/constants.js');
+const createApiUrl = require('./../helpers/create-api-url.js');
+
+const getUserInfo = (userName) => {
+  const userInfoQuery = `user=${userName}`;
+  return createApiUrl(constants.rootUrl, constants.methods.getUserInfo, process.env.API_KEY, userInfoQuery);
+}
+
+module.exports = getUserInfo;
