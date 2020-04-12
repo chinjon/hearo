b6759df39049adc3cb95554096ef69280443cdca
Jonathan Chin <jonathankchin@gmail.com>
2020-04-12 10:34:41 -0400

    doc: update module comments

--- utils/helpers/create-api-url.js
+++ utils/helpers/create-api-url.js
@@ -1,10 +1,11 @@
 /**
  * @description Helper function that creates a lastfm api url
- * @param {String} rootUrl 
- * @param {String} searchMethod 
- * @param {String} apiKey 
- * @param {String} searchQuery 
- * @param {String} format 
+ * @param {String} rootUrl Base API url
+ * @param {String} searchMethod Native search methods from LastFM API
+ * @param {String} apiKey Unique API key
+ * @param {String} searchQuery Query parameters for search query
+ * @param {String} format Format of output: either JSON or XML
+ * @returns {String} Returns LastFM API URL to execute API call
  */
 const createApiUrl = (rootUrl, searchMethod, apiKey, searchQuery, format = 'json') => {
   return `${rootUrl}method=${searchMethod}&api_key=${apiKey}&${searchQuery}&format=${format}`;
