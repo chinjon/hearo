6a317b3b9f9f454fe42be7c436bffb909509a3d8
Jonathan Chin <jonathankchin@gmail.com>
2020-04-12 19:39:21 -0400

    feat: add getartistinfo method

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,6 +1,9 @@
 module.exports = {
   rootUrl: 'http://ws.audioscrobbler.com/2.0/?',
   methods: {
+    artist:{
+      getArtistInfo: 'artist.getinfo'
+    },
     user: {
       getUserInfo: 'user.getinfo'
     }
