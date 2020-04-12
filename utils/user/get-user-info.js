3967fadfad08ffe3e783764e7dbcdc973d73d58a
Jonathan Chin <jonathankchin@gmail.com>
2020-04-12 00:57:45 -0400

    feat: update object reference

--- utils/user/get-user-info.js
+++ utils/user/get-user-info.js
@@ -4,7 +4,7 @@ const createApiUrl = require('./../helpers/create-api-url.js');
 
 const getUserInfo = (userName) => {
   const userInfoQuery = `user=${userName}`;
-  return createApiUrl(constants.rootUrl, constants.methods.getUserInfo, process.env.API_KEY, userInfoQuery);
+  return createApiUrl(constants.rootUrl, constants.methods.user.getUserInfo, process.env.API_KEY, userInfoQuery);
 }
 
 module.exports = getUserInfo;
