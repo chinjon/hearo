0afef0fb42e60ad4def0920ff082d2cd7cab9335
Jonathan Chin <jonathankchin@gmail.com>
2020-04-12 10:29:42 -0400

    doc: add method comment

--- utils/user/get-user-info.js
+++ utils/user/get-user-info.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 const constants = require('./../../data/constants.js');
 const createApiUrl = require('./../helpers/create-api-url.js');
 
+/**
+ * @description Abstraction of `user.getinfo` method
+ * @param {String} userName User name to get information on
+ * @returns {Object} 
+ */
 const getUserInfo = (userName) => {
   const userInfoQuery = `user=${userName}`;
   return createApiUrl(constants.rootUrl, constants.methods.user.getUserInfo, process.env.API_KEY, userInfoQuery);
