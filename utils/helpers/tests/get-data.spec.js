ab3b3b2eef1d9feb0a41d015a3a3b5ea118a61ab
Jonathan Chin <jonathankchin@gmail.com>
2020-04-12 15:00:02 -0400

    test: update test to assert fail cases

--- utils/helpers/tests/get-data.spec.js
+++ utils/helpers/tests/get-data.spec.js
@@ -5,23 +5,38 @@ const constants = require('../../../data/constants.js');
 const createApiUrl = require('../create-api-url.js');
 
 describe('getData()', () => {
-  const API_URL = createApiUrl(
-    constants.rootUrl,
-    constants.methods.user.getUserInfo,
-    process.env.API_KEY,
-    '&user=sharonvanetten',
-    'json'
-  );
+  const TEST_CONSTANTS = {
+    SUCCESS: createApiUrl(
+      constants.rootUrl,
+      constants.methods.user.getUserInfo,
+      process.env.API_KEY,
+      '&user=sharonvanetten',
+      'json'
+    ),
+    FAIL: createApiUrl(
+      constants.rootUrl,
+      constants.methods.user.getUserInfo,
+      12345,
+      '&user=sharonvanetten',
+      'json'
+    )
+  }
 
   it('should return value of type object', () => {
-    getData(API_URL).then((data) => {
+    getData(TEST_CONSTANTS.SUCCESS).then((data) => {
       assert.deepStrictEqual(typeof data, 'object');
     });
   });
 
-  it('should return a response of 200', () => {
-    getData(API_URL).then((data) => {
+  it('should return a response of 200 with valid API key', () => {
+    getData(TEST_CONSTANTS.SUCCESS).then((data) => {
       assert.deepStrictEqual(data.status, 200);
+    });
+  });
+
+  it('should return a response of 400 with invalid API key', () => {
+    getData(TEST_CONSTANTS.FAIL).then((data) => {
+      assert.deepStrictEqual(data.status >= 400, true);
     });
   });
 });
