4c86647c5e0a3cb3d357d97c15a83d694b6a79ac
Jonathan Chin <jonathankchin@gmail.com>
2020-04-19 16:59:55 -0400

    test: add assertions to test for undefined

--- src/lib/artist/tests/get-artist-info.spec.js
+++ src/lib/artist/tests/get-artist-info.spec.js
@@ -7,8 +7,16 @@ describe('getArtistInfo()', () => {
   it('should return a value of type string', () => {
     assert.deepStrictEqual(typeof API_URL, 'string');
   });
+  
+  it('should include the correct method in API url', () => {
+    assert.doesNotMatch(API_URL, /&method=undefined/);
+  });
 
   it('should include the correct search query', () => {
     assert.match(API_URL, /&artist=radiohead/);
+  });
+
+  it('should include "&artist=undefined" in search query', () => {
+    assert.doesNotMatch(API_URL, /&artist=undefined/);
   });
 });
--- src/lib/artist/tests/get-artist-top-albums.spec.js
+++ src/lib/artist/tests/get-artist-top-albums.spec.js
@@ -8,7 +8,15 @@ describe('getArtistTopAlbums()', () => {
     assert.deepStrictEqual(typeof API_URL, 'string');
   });
 
+  it('should include the correct method in API url', () => {
+    assert.doesNotMatch(API_URL, /&method=undefined/);
+  });
+
   it('should include the correct search query', () => {
     assert.match(API_URL, /&artist=radiohead/);
+  });
+
+  it('should include "&artist=undefined" in search query', () => {
+    assert.doesNotMatch(API_URL, /&artist=undefined/);
   });
 });
