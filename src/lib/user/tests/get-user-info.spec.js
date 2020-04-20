26b4518f8d1ec7be721936933e41315206d55ae0
Jonathan Chin <jonathankchin@gmail.com>
2020-04-19 21:35:42 -0400

    test: update assertion to test url output

--- src/lib/user/tests/get-user-info.spec.js
+++ src/lib/user/tests/get-user-info.spec.js
@@ -1,13 +1,15 @@
 const assert = require('assert');
 const getUserInfo = require('../get-user-info.js');
 
-const TEST_USER_NAME = 'testusername';
-
 describe('getUserInfo()', () => {
-  const API_URL = getUserInfo(TEST_USER_NAME);
+  const API_URL = getUserInfo('testusername');
 
   it('should return value of type string', () => {
     assert.deepStrictEqual(typeof API_URL, 'string');
+  });
+
+  it('should include the correct method in API url', () => {
+    assert.doesNotMatch(API_URL, /&method=undefined/);
   });
 
   it('should include the correct search query', () => {
--- src/lib/user/tests/get-user-recent-tracks.spec.js
+++ src/lib/user/tests/get-user-recent-tracks.spec.js
@@ -1,13 +1,15 @@
 const assert = require('assert');
 const getUserRecentTracks = require('../get-user-recent-tracks.js');
 
-const TEST_USER_NAME = 'testusername';
-
 describe('getUserRecentTracks()', () => {
-  const API_URL = getUserRecentTracks(TEST_USER_NAME);
+  const API_URL = getUserRecentTracks('testusername');
 
   it('should return value of type string', () => {
     assert.deepStrictEqual(typeof API_URL, 'string');
+  });
+
+  it('should include the correct method in API url', () => {
+    assert.doesNotMatch(API_URL, /&method=undefined/);
   });
 
   it('should include the correct search query', () => {
