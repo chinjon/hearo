0a4c4e96ca0b8d3a29c4ac6e6818f4ef912a45b1
Jonathan Chin <jonathankchin@gmail.com>
2020-04-21 21:51:56 -0400

    feat: add top artists to main script

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 
 const getData = require('./lib/helpers/get-data.js');
 const getUserInfo = require("./lib/user/get-user-info.js");
+const getUserTopArtists = require("./lib/user/get-user-top-artists.js");
 
 class Hearo {
   constructor(API_KEY) {
@@ -11,7 +12,19 @@ class Hearo {
   userInfo(userName) {
     getData(getUserInfo(userName))
       .then((res) => res.json())
-      .then((json) => json);
+      .then((json) => json)
+      .catch((error) => {
+        throw new Error(error);
+      });
+  }
+
+  userTopArtists(userName) {
+    getData(getUserTopArtists(userName))
+      .then((res) => res.json())
+      .thne((json) => json)
+      .catch((error) => {
+        throw new Error(error);
+      });
   }
 }
 
